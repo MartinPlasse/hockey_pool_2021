b315f7f46cba848d5448bec04303d1d3653b9d88
Mart <martinplasse86@gmail.com>
2021-01-21 21:42:01 -0500

    penis

--- hockey_pool/pool_lookup.py
+++ hockey_pool/pool_lookup.py
@@ -1,10 +1,9 @@
 import re
-from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
-from googlesearch import search
+from selenium import webdriver
 
 
 def is_wipo(url):
@@ -27,31 +26,29 @@ def contains_name(htmlelement, name):
     return False
 
 
-
-
 def lookup_pool(contender_name):
-            webpage = requests.get(url)
-            soup = BeautifulSoup(webpage.content, "html.parser")
-            elements = soup.find_all(attrs={"class": "biblio-person-list--name"})
+    webpage = requests.get("https://poolhockey.lapresse.ca/groups_03.php")
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    elements = soup.find_all(attrs={"class": "tt12b_n"})
 
-            # 3. Extract the applicant name text from the html element
-            for element in elements:
-                shaved_element = re.findall(r"(?:<.*>\w+)(.*)", str(element))
-                if fuzz.token_sort_ratio(shaved_element, english_name) >= 88:
-                    text = element.get_text()
+    # 3. Extract the applicant name text from the html element
+    for element in elements:
+        shaved_element = re.findall(r"(?:<.*>\w+)(.*)", str(element))
+        if fuzz.token_sort_ratio(shaved_element, contender_name) >= 88:
+            text = element.get_text()
 
-                    chinese_name = ""
-                    for character in range(len(text)):
-                        if detect_chinese(text[character]) is True:
-                            chinese_name += text[character]
+            chinese_name = ""
+            for character in range(len(text)):
+                if detect_chinese(text[character]) is True:
+                    chinese_name += text[character]
 
-                    if chinese_name == "":
-                        chinese_name = None
-                    break
-            else:
-                continue
-            return chinese_name
+            if chinese_name == "":
+                chinese_name = None
+            break
+    else:
+        continue
+        return chinese_name
 
 
 if __name__ == "__main__":
-    # test1 = lookup_chinese("QIQIHAR RAILWAY ROLLING STOCK CO LTD")
+    test1 = lookup_pool("QIQIHAR RAILWAY ROLLING STOCK CO LTD")
