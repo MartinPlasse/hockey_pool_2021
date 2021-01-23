9034619dc0465e1830cb72c194627893eb46a4c1
Mart <martinplasse86@gmail.com>
2021-01-23 13:44:27 -0500

    test site_login function for lapresse hockey pool site

---
+++ hockey_pool/site_login_test.py
@@ -0,0 +1,11 @@
+from site_login import site_login
+
+url = "https://poolhockey.lapresse.ca/login.php"
+username = "Martinal86"
+password = "DoX2X17s$$$"
+idun = "login"
+idpw = "pass"
+idbutton = "Entrer"
+
+
+site_login(url, username, password, idun, idpw, idbutton)
